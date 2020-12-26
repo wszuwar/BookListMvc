a83e7f44db40244f5ec8e9e506c0a7a98b0cc402
wszuwar <msuwr315@gmail.com>
2020-12-27 00:01:40 +0100

    Add project files.

---
+++ BookListMvc/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListMvc.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { set; get; }
+        [Required]
+        public string Name { set; get; }
+
+        public string Author { set; get; }
+
+        public string ISBN { set; get; }
+
+
+    }
+}
